f70790f5755cc2b35b6b20450a86fe6b22ae943c
ThorvaldBoe <boe74@hotmail.com>
2017-01-18 22:35:31 +0100

    First version, simple single list copy schema function is working

--- SPCopyListLib/SPCopyListLib.cs
+++ SPCopyListLib/SPCopyListLib.cs
@@ -1,6 +1,12 @@
-﻿using System;
+﻿using Microsoft.SharePoint.Client;
+using OfficeDevPnP.Core.Framework.Provisioning.Model;
+using OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers;
+using OfficeDevPnP.Core.Framework.Provisioning.Providers.Json;
+using OfficeDevPnP.Core.Framework.Provisioning.Providers.Xml;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,10 +14,231 @@ namespace SPCopyListLib
 {
     public class SPCopyListLib
     {
-        public void SPCopyList(string siteSource, string siteTarget, string listName)
+        public string SourceSite { get; set; }
+        public string TargetSite { get; set; }
+        public ClientContext SourceContext { get; set; }
+        public ClientContext TargetContext { get; set; }
+        private const int MAX_ITEMS = 2000;
+
+        public enum SPCopyListCopyOptions {
+            AbortOnDuplicate, Rename, Overwrite, Merge
+        }
+
+        public SPCopyListCopyResult SPCopyList(string sourceSite, string targetSite, string listName)
         {
+            return SPCopyList(sourceSite, targetSite, listName, SPCopyListCopyOptions.Merge);
 
         }
+
+        public SPCopyListCopyResult SPCopyList(string sourceSite, string targetSite, string listName, SPCopyListCopyOptions options)
+        {
+            SPCopyListCopyResult result = new SPCopyListCopyResult();
+            CheckConnected();
+
+            Web web = SourceContext.Web;
+            ProvisioningTemplateCreationInformation ptci = new ProvisioningTemplateCreationInformation(web);
+            JsonTemplateProvider jsonProvider = new JsonFileSystemTemplateProvider();
+            var template = web.GetProvisioningTemplate();
+
+            template.Lists.RemoveAll(l => l.Title != listName);
+
+            Web targetWeb = TargetContext.Web;
+            var ptai = new ProvisioningTemplateApplyingInformation();
+            ptai.HandlersToProcess = Handlers.Lists;
+
+            TargetContext.Load(targetWeb.Lists);
+            TargetContext.ExecuteQueryRetry();
+
+            bool listExists = ListExists(listName, targetWeb.Lists);
+
+            //TODO - remove
+            var debugList = template.Lists.Find(l => l.Title == listName);
+
+
+            if (options == SPCopyListCopyOptions.AbortOnDuplicate)
+            {
+                if (listExists)
+                {
+                    result.Conflict = true;
+                    result.ListCopied = false;
+                    result.ConflictAction = SPCopyListCopyResult.SPCopyListConflictAction.Aborted;
+                } else
+                {
+                    result.Conflict = false;
+                    result.ListCopied = true;
+                    result.ConflictAction = SPCopyListCopyResult.SPCopyListConflictAction.None;
+
+                    targetWeb.ApplyProvisioningTemplate(template, ptai);
+                }
+            }
+            else if (options == SPCopyListCopyOptions.Merge)
+            {
+                if (listExists)
+                {
+                    result.Conflict = true;
+                    result.ListCopied = true;
+                    result.ConflictAction = SPCopyListCopyResult.SPCopyListConflictAction.Merged;
+                }
+                else
+                {
+                    result.Conflict = false;
+                    result.ListCopied = true;
+                    result.ConflictAction = SPCopyListCopyResult.SPCopyListConflictAction.None;
+                }
+
+                targetWeb.ApplyProvisioningTemplate(template, ptai);
+
+            }
+            else if (options == SPCopyListCopyOptions.Overwrite)
+            {
+                if (listExists)
+                {
+                    result.Conflict = true;
+                    result.ListCopied = true;
+                    result.ConflictAction = SPCopyListCopyResult.SPCopyListConflictAction.Overwritten;
+
+                    DeleteTargetList(targetWeb, listName);
+                }
+                else
+                {
+                    result.Conflict = false;
+                    result.ListCopied = true;
+                    result.ConflictAction = SPCopyListCopyResult.SPCopyListConflictAction.None;
+                }
+
+                targetWeb.ApplyProvisioningTemplate(template, ptai);
+
+            }
+            else if (options == SPCopyListCopyOptions.Rename)
+            {
+                if (listExists)
+                {
+                    result.Conflict = true;
+                    result.ListCopied = true;
+                    result.ConflictAction = SPCopyListCopyResult.SPCopyListConflictAction.Renamed;
+
+                    var list = template.Lists.Find(l => l.Title == listName);
+                    list.Title = GenerateUniqueListName(targetWeb.Lists, listName);
+                    list.Url = AlignUrlWithTitle(list.Title, list.Url);
+                    
+                }
+                else
+                {
+                    result.Conflict = false;
+                    result.ListCopied = true;
+                    result.ConflictAction = SPCopyListCopyResult.SPCopyListConflictAction.None;
+
+                }
+
+                targetWeb.ApplyProvisioningTemplate(template, ptai);
+                
+
+            }
+
+            return result;
+        }
+
+        private string AlignUrlWithTitle(string title, string url)
+        {
+            string result = "";
+            int slashIndex = url.LastIndexOf("/");
+            if (slashIndex == -1)
+            {
+                result = title;
+            }
+            else
+            {
+                string firstPart = url.Substring(0, slashIndex);
+                string secondPart = title;
+                result = firstPart + "/" + secondPart;
+            }
+
+            return result;
+        }
+
+        private string GenerateUniqueListName(ListCollection lists, string listName)
+        {
+            string result = listName;
+            if (!ListExists(listName, lists)) return listName;
+
+            for (int i = 1; i <= (MAX_ITEMS + 1); i++)
+            {
+                result = listName + i.ToString();
+
+                if (i == (MAX_ITEMS + 1)) throw new Exception("Max number of lists reached");
+
+                if (!ListExists(result, lists))
+                    break;
+            }
+            
+            return result;
+        }
+
+        private void DeleteTargetList(Web targetWeb, string listName)
+        {
+            List oList = targetWeb.Lists.GetByTitle(listName);
+            oList.DeleteObject();
+            TargetContext.ExecuteQueryRetry();
+        }
+
+        private bool ListExists(string listName, ListCollection lists)
+        {
+            bool result = false;
+            foreach (var item in lists)
+            {
+                if (item.Title == listName)
+                {
+                    result = true;
+                    break;
+                }
+            }
+            return result;
+        }
+
+        private void CheckConnected()
+        {
+            if (SourceContext == null || TargetContext == null) throw new Exception("Source or Target context is not connected");
+        }
+
+        public void ConnectSource(string site, string user, string password)
+        {
+            SourceSite = site;
+            SourceContext = new ClientContext(site);
+            SecureString spwd = new SecureString();
+            foreach (char c in password.ToCharArray()) spwd.AppendChar(c);
+            SourceContext.Credentials = new SharePointOnlineCredentials(user, spwd);
+            Web web = SourceContext.Web;
+            SourceContext.ExecuteQueryRetry();
+        }
+
+        public void ConnectTarget(string site, string user, string password)
+        {
+            TargetSite = site;
+            TargetContext = new ClientContext(site);
+            SecureString spwd = new SecureString();
+            foreach (char c in password.ToCharArray()) spwd.AppendChar(c);
+            TargetContext.Credentials = new SharePointOnlineCredentials(user, spwd);
+            Web web = TargetContext.Web;
+            TargetContext.ExecuteQueryRetry();
+        }
+
+    }
+
+    public class SPCopyListCopyResult
+    {
+        public SPCopyListCopyResult()
+        {
+            ConflictAction = SPCopyListConflictAction.None;
+        }
+
+        public enum SPCopyListConflictAction
+        {
+            None, Aborted, Renamed, Overwritten, Merged
+        }
+
+        public bool ListCopied { get; set; }
+        public bool Conflict { get; set; }
+        public SPCopyListConflictAction ConflictAction { get; set; }
 
     }
 }
--- SPCopyListLibTests/SPCopyListLibTests.cs
+++ SPCopyListLibTests/SPCopyListLibTests.cs
@@ -2,6 +2,7 @@
 using SPCopyListLib;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,11 +12,157 @@ namespace SPCopyListLib.Tests
     [TestClass()]
     public class SPCopyListLibTests
     {
+        [ClassInitialize]
+        public static void Initialize(TestContext context)
+        {
+            
+        }
+
+        [TestMethod()]
+        public void SPCopyListAdhocTest()
+        {
+            var lib = SetupSPCopyListInstance();
+            lib.SPCopyList(lib.SourceSite, lib.TargetSite, "TestList1");
+
+        }
+
+        private SPCopyListLib SetupSPCopyListInstance()
+        {
+            SPCopyListLib lib = new SPCopyListLib();
+            string sourceSiteBase = ConfigurationManager.AppSettings["sourceSiteBase"];
+            string sourceSite = sourceSiteBase + "/source1";
+            string targetSite = sourceSiteBase + "/target1";
+            
+            string userNameSource = ConfigurationManager.AppSettings["sourceSiteUser"];
+            string userNameTarget = ConfigurationManager.AppSettings["targetSiteUser"];
+            string passwordSource = ConfigurationManager.AppSettings["sourceSitePassword"];
+            string passwordTarget = ConfigurationManager.AppSettings["targetSitePassword"];
+
+            lib.ConnectSource(sourceSite, userNameSource, passwordSource);
+            lib.ConnectTarget(targetSite, userNameTarget, passwordTarget);
+
+            return lib;
+        }
+
         [TestMethod()]
         public void SPCopyListTest()
         {
             SPCopyListLib lib = new SPCopyListLib();
-            lib.SPCopyList("", "", "");
+            string sourceSiteBase = ConfigurationManager.AppSettings["sourceSiteBase"];
+            string sourceSite = sourceSiteBase + "/source1";
+            string targetSite = sourceSiteBase + "/target1";
+            string listName = "TestList1";
+
+            try
+            {
+                lib.SPCopyList(sourceSite, targetSite, listName);
+            } catch (Exception ex)
+            {
+                Assert.AreEqual("Source or Target context is not connected", ex.Message);
+            }
+
+            string userNameSource = ConfigurationManager.AppSettings["sourceSiteUser"];
+            string userNameTarget = ConfigurationManager.AppSettings["targetSiteUser"];
+            string passwordSource = ConfigurationManager.AppSettings["sourceSitePassword"];
+            string passwordTarget = ConfigurationManager.AppSettings["targetSitePassword"];
+            lib.ConnectSource(sourceSite, userNameSource, passwordSource);
+
+            try
+            {
+                lib.SPCopyList(sourceSite, targetSite, listName);
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual("Source or Target context is not connected", ex.Message);
+            }
+
+            lib.ConnectTarget(targetSite, userNameTarget, passwordTarget);
+            var copyResult = lib.SPCopyList(sourceSite, targetSite, listName);
+            if (copyResult.Conflict)
+                Assert.AreEqual(SPCopyListCopyResult.SPCopyListConflictAction.Merged, copyResult.ConflictAction);
+            else
+                Assert.AreEqual(SPCopyListCopyResult.SPCopyListConflictAction.None, copyResult.ConflictAction);
+
+
+            //the list now exists in both sites. Let's test conflict handling:
+
+            //Abort
+            copyResult = lib.SPCopyList(sourceSite, targetSite, listName, SPCopyListLib.SPCopyListCopyOptions.AbortOnDuplicate);
+            Assert.AreEqual(SPCopyListCopyResult.SPCopyListConflictAction.Aborted, copyResult.ConflictAction);
+            Assert.AreEqual(true, copyResult.Conflict);
+            Assert.AreEqual(false, copyResult.ListCopied);
+
+            //Merge
+            copyResult = lib.SPCopyList(sourceSite, targetSite, listName, SPCopyListLib.SPCopyListCopyOptions.Merge);
+            Assert.AreEqual(SPCopyListCopyResult.SPCopyListConflictAction.Merged, copyResult.ConflictAction);
+            Assert.AreEqual(true, copyResult.Conflict);
+            Assert.AreEqual(true, copyResult.ListCopied);
+
+            //Overwrite
+            copyResult = lib.SPCopyList(sourceSite, targetSite, listName, SPCopyListLib.SPCopyListCopyOptions.Overwrite);
+            Assert.AreEqual(SPCopyListCopyResult.SPCopyListConflictAction.Overwritten, copyResult.ConflictAction);
+            Assert.AreEqual(true, copyResult.Conflict);
+            Assert.AreEqual(true, copyResult.ListCopied);
+
+            //Rename
+            copyResult = lib.SPCopyList(sourceSite, targetSite, listName, SPCopyListLib.SPCopyListCopyOptions.Rename);
+            Assert.AreEqual(SPCopyListCopyResult.SPCopyListConflictAction.Renamed, copyResult.ConflictAction);
+            Assert.AreEqual(true, copyResult.Conflict);
+            Assert.AreEqual(true, copyResult.ListCopied);
+
+        }
+
+        [TestMethod()]
+        public void CopyLookupListTest()
+        {
+            SPCopyListLib lib = new SPCopyListLib();
+            string sourceSiteBase = ConfigurationManager.AppSettings["sourceSiteBase"];
+            string sourceSite = sourceSiteBase + "/source1";
+            string targetSite = sourceSiteBase + "/target1";
+            string listName = "MyCustomLookup"; //a list with a lookup field
+
+            string userNameSource = ConfigurationManager.AppSettings["sourceSiteUser"];
+            string userNameTarget = ConfigurationManager.AppSettings["targetSiteUser"];
+            string passwordSource = ConfigurationManager.AppSettings["sourceSitePassword"];
+            string passwordTarget = ConfigurationManager.AppSettings["targetSitePassword"];
+            lib.ConnectSource(sourceSite, userNameSource, passwordSource);
+            lib.ConnectTarget(targetSite, userNameTarget, passwordTarget);
+
+            //TODO: Add handling for lookup fields
+            //var copyResult = lib.SPCopyList(sourceSite, targetSite, listName);
+        }
+
+        [TestMethod()]
+        public void ConnectSourceTest()
+        {
+            SPCopyListLib lib = new SPCopyListLib();
+            Assert.IsNull(lib.SourceContext);
+            Assert.IsNull(lib.TargetContext);
+
+            string sourceSiteBase = ConfigurationManager.AppSettings["sourceSiteBase"];
+            string user = ConfigurationManager.AppSettings["sourceSiteUser"];
+            string password = ConfigurationManager.AppSettings["sourceSitePassword"];
+            lib.ConnectSource(sourceSiteBase, user, password);
+
+            Assert.IsNotNull(lib.SourceContext);
+            Assert.AreEqual(sourceSiteBase, lib.SourceSite);
+        }
+
+        [TestMethod()]
+        public void ConnectTargetTest()
+        {
+            SPCopyListLib lib = new SPCopyListLib();
+            Assert.IsNull(lib.TargetContext);
+            Assert.IsNull(lib.TargetContext);
+
+            string targetSiteBase = ConfigurationManager.AppSettings["targetSiteBase"];
+            string user = ConfigurationManager.AppSettings["targetSiteUser"];
+            string password = ConfigurationManager.AppSettings["targetSitePassword"];
+            lib.ConnectTarget(targetSiteBase, user, password);
+
+            Assert.IsNotNull(lib.TargetContext);
+            Assert.AreEqual(targetSiteBase, lib.TargetSite);
+
 
         }
     }
